46b0e22b4eb4d3b014cf24d0133e5dad1516dba0
MdFawazAli <Fawazzali786@gmail.com>
2025-07-25 20:52:47 +0530

    Express js by traversy media

---
+++ server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+const app = express();
+const port = 8000;
+
+app.get('/', (req,res) =>{
+    res.json({
+        msg: 'hello world'
+    })
+})
+
+app.listen(port, () =>{
+    console.log(`listening on http://localhost:${port}`)
+})
