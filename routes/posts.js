1935b2e2df4d4905e8b9ec9549592a1d3c338cc9
MdFawazAli <Fawazzali786@gmail.com>
2025-07-26 21:48:03 +0530

    traversy media express

--- routes/posts.js
+++ routes/posts.js
@@ -8,7 +8,7 @@ let posts = [
 ]
 
 // To get all the posts and set limit of posts
-router.get('/', (req, res) => {
+router.get('/', (req, res, next) => {
     let limit = parseInt(req.query.limit)
     if (!isNaN(limit) && limit > 0) {
         return res.status(200).json(posts.slice(0, limit))
@@ -17,51 +17,59 @@ router.get('/', (req, res) => {
 })
 
 // To get only one post 
-router.get('/:id', (req, res) => {
+router.get('/:id', (req, res, next) => {
     let id = parseInt(req.params.id);
     let post = posts.find((post) => {
         return post.id === id;
     })
     if (!post) {
-        res.status(404).json({ msg: `${id} is not found in the posts` })
+        const error = new Error(`${id} is not found in the posts`);
+        error.status = 404;
+        return next(error)
     }
     res.status(200).json(post);
 })
 
 // Create a new post by .POST method
-router.post('/', (req, res) => {
+router.post('/', (req, res, next) => {
     const newpost = {
         id: posts.length + 1,
         title: req.body.title
     }
     if (!newpost.title) {
-        res.status(400).json({ msg: 'pls include the title' })
+        const error = new Error(`Pls include the title`);
+        error.status = 404;
+        return next(error)
     }
     posts.push(newpost)
     res.status(201).json(posts)
 })
 
 // update a post using .put method
-router.put('/:id', (req, res) => {
+router.put('/:id', (req, res, next) => {
     const id = parseInt(req.params.id);
     const post = posts.find((post) => {
         return post.id === id;
     })
     if (!post) {
-        res.status(404).json({ msg: `${id} is not found in the posts` })
+        const error = new Error(`${id} is not found in the posts`);
+        error.status = 404;
+        return next(error)
     }
     post.title = req.body.title
     res.status(200).json(posts)
 })
 
 // Delete a post using .delete method
-router.delete('/:id', (req, res) => {
+router.delete('/:id', (req, res, next) => {
     const id = parseInt(req.params.id);
     const post = posts.find((post) => {
         return post.id === id;
     })
     if (!post) {
-        res.status(404).json({ msg: `${id} is not found in the posts` })
+        const error = new Error(`${id} is not found in the posts`);
+        error.status = 404;
+        return next(error)
     } else {
         res.status(200).json(posts.filter((post) => {
             return post.id != id;
--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const app = express();
 const port = 8000;
 import posts from './routes/posts.js';
 import logger from './middleware/logger.js'
+import errorhandler from './middleware/error.js';
 
-// exporting middleware
+
+// exporting middleware logger
 app.use(logger)
 
 // body parserer middleware
@@ -16,6 +18,9 @@ app.use(express.static('public'));
 
 // routes
 app.use('/api/posts', posts);
+
+// exporting middleware errorhadnler
+app.use(errorhandler)
 
 app.listen(port, () =>{
     console.log(`listening on http://localhost:${port}`);
